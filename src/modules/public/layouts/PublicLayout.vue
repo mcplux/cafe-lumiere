06d5e3a0a20c7fd64adebeedff5c0e5e3a211b7c
Juan Martinez <pablomar01@outlook.com>
2025-04-07 08:58:23 -0500

    Create PublicLayout

---
+++ src/modules/public/layouts/PublicLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="sticky top-0 items-center bg-gray-600 text-white p-5">
+    <h1 class="text-4xl font-bold">Café Lumière</h1>
+  </header>
+
+  <main class="max-w-4xl mx-auto mt-10">
+    <RouterView />
+  </main>
+
+  <footer class="text-center text-gray-400 text-sm mt-10">
+    &copy; Cafe Lumiere. All rights reserved {{ new Date().getFullYear() }}
+  </footer>
+</template>
---
+++ src/modules/public/routes/index.ts
@@ -0,0 +1,17 @@
+import type { RouteRecordRaw } from 'vue-router'
+import PublicLayout from '../layouts/PublicLayout.vue'
+
+export const publicRoutes: RouteRecordRaw = {
+  path: '/',
+  component: PublicLayout,
+  children: [],
+}
+
+// {
+//   path: '/about',
+//   name: 'about',
+//   // route level code-splitting
+//   // this generates a separate chunk (About.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () => import('../views/AboutView.vue'),
+// },
