208a20753cdbde065e5464fe5559e3a7e7b6e721
Juan Martinez <pablomar01@outlook.com>
2025-04-15 11:14:20 -0500

    Added menu and order preview to new order view

--- src/modules/orders/stores/orders.store.ts
+++ src/modules/orders/stores/orders.store.ts
@@ -3,9 +3,30 @@ import { defineStore } from 'pinia'
 
 import { getOrdersAction } from '../actions/get-orders.action'
 import type { OrderResponse } from '../interfaces/order.response'
+import type { MenuItem } from '@/modules/menu/interfaces'
+
+interface OrderItem {
+  menuItem: MenuItem
+  quantity: number
+}
 
 export const useOrdersStore = defineStore('orders', () => {
   const orders = ref<OrderResponse[]>([])
+  const orderItems = ref<OrderItem[]>([])
+
+  const addOrderItem = (menuItem: MenuItem) => {
+    const orderItem = orderItems.value.find((item) => item.menuItem.id === menuItem.id)
+
+    if (orderItem) {
+      orderItems.value = orderItems.value.map((item) => {
+        return item.menuItem.id === menuItem.id ? { ...item, quantity: item.quantity + 1 } : item
+      })
+    } else {
+      orderItems.value.push({ menuItem, quantity: 1 })
+    }
+
+    console.log(orderItems.value)
+  }
 
   const getTodayOrders = async () => {
     const now = new Date()
@@ -22,5 +43,7 @@ export const useOrdersStore = defineStore('orders', () => {
 
   return {
     orders,
+    orderItems,
+    addOrderItem,
   }
 })
--- src/modules/waiter/views/WaiterNewOrderView.vue
+++ src/modules/waiter/views/WaiterNewOrderView.vue
@@ -1,42 +1,101 @@
 <script setup lang="ts">
 import { formatCurrency } from '@/modules/common/helpers'
 import { useMenuStore } from '@/modules/menu/stores/menu.store'
+import { useOrdersStore } from '@/modules/orders/stores/orders.store'
 
 const menuStore = useMenuStore()
+const ordersStore = useOrdersStore()
 </script>
 
 <template>
   <h1 class="text-3xl text-center font-bold">New Order</h1>
 
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-10">
-    <div
-      v-for="item in menuStore.menuItems"
-      :key="item.id"
-      class="relative border border-gray-200 p-2"
-    >
-      <button
-        class="absolute -right-3 -top-3 bg-orange-400 text-white rounded-full p-2"
-        @click="console.log('Hello world!')"
+  <div class="mt-10">
+    <h2 class="text-xl font-bold">Order Preview</h2>
+
+    <div class="flex flex-col gap-3 mt-5">
+      <div
+        v-for="item in ordersStore.orderItems"
+        :key="item.menuItem.id"
+        class="relative border border-gray-200 p-2 flex flex-col sm:flex-row gap-3"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
+        <button
+          class="absolute -right-3 -top-3 bg-red-400 text-white rounded-full p-2"
+          @click="() => console.log('Hello world!')"
         >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-        </svg>
-      </button>
-      <div class="w-full h-64 overflow-hidden">
-        <img
-          src="https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          class="w-full h-full object-cover object-center"
-        />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </button>
+
+        <div class="w-full sm:w-64 overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            class="w-full h-full object-cover object-center"
+          />
+        </div>
+
+        <div>
+          <p class="font-bold text-lg">{{ item.menuItem.name }}</p>
+          <p>
+            Price:
+            <span class="font-bold"
+              >{{ formatCurrency(item.menuItem.price) }} x {{ item.quantity }}</span
+            >
+          </p>
+          <div class="flex gap-2">
+            <button class="bg-gray-200 px-2 rounded">-1</button>
+            <button class="bg-gray-200 px-2 rounded">+1</button>
+          </div>
+          <p>
+            Total:
+            <span class="font-bold">{{ formatCurrency(item.menuItem.price * item.quantity) }}</span>
+          </p>
+        </div>
       </div>
-      <p class="text-lg font-bold">{{ item.name }}</p>
-      <p class="text-lg font-bold">{{ formatCurrency(item.price) }}</p>
+    </div>
+  </div>
+
+  <div class="mt-10">
+    <h2 class="text-xl font-bold">Menu</h2>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-5">
+      <div
+        v-for="item in menuStore.menuItems"
+        :key="item.id"
+        class="relative border border-gray-200 p-2"
+      >
+        <button
+          class="absolute -right-3 -top-3 bg-orange-400 text-white rounded-full p-2"
+          @click="() => ordersStore.addOrderItem(item)"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+        </button>
+        <div class="w-full h-64 overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            class="w-full h-full object-cover object-center"
+          />
+        </div>
+        <p class="text-lg font-bold">{{ item.name }}</p>
+        <p class="text-lg font-bold">{{ formatCurrency(item.price) }}</p>
+      </div>
     </div>
   </div>
 </template>
