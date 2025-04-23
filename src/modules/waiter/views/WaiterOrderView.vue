23ade616b621b5589ddae7f6836d44a9bbfb0632
Juan Martinez <pablomar01@outlook.com>
2025-04-22 20:40:56 -0500

    Add the order information to  WaiterOrderView

--- src/modules/waiter/views/WaiterOrderView.vue
+++ src/modules/waiter/views/WaiterOrderView.vue
@@ -4,6 +4,7 @@ import { useRoute } from 'vue-router'
 
 import type { OrderResponse } from '@/modules/orders/interfaces'
 import { useOrdersStore } from '@/modules/orders/stores/orders.store'
+import { formatCurrency, formatDate, getOrderTotalAmount } from '@/modules/common/helpers'
 
 const route = useRoute()
 const ordersStore = useOrdersStore()
@@ -17,7 +18,74 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1 class="text-3xl text-center font-bold">Order {{ id.substring(0, 8) }}</h1>
+  <h1 class="text-3xl text-center font-bold">Order - {{ id.substring(0, 8) }}</h1>
 
-  <pre>{{ order }}</pre>
+  <div v-if="order" class="mt-10">
+    <div class="flex flex-col gap-3 border border-gray-700 rounded p-5">
+      <p class="text-lg">
+        ID: <span class="font-bold">{{ order.id }}</span>
+      </p>
+      <p class="text-lg">
+        Client: <span class="font-bold">{{ order.client }}</span>
+      </p>
+      <p class="text-lg">
+        Date: <span class="font-bold">{{ formatDate(order.createdAt) }}</span>
+      </p>
+
+      <p class="text-lg">
+        Status: <span class="font-bold">{{ order.orderStatus.toUpperCase() }}</span>
+      </p>
+
+      <p class="text-lg">
+        Total: <span class="font-bold">{{ getOrderTotalAmount(order) }}</span>
+      </p>
+
+      <p class="text-lg">
+        Aditional Notes: <span class="font-bold">{{ order.notes ?? '' }}</span>
+      </p>
+    </div>
+
+    <div class="mt-5 border border-gray-700 rounded p-5">
+      <h3 class="text-lg font-bold">Items:</h3>
+
+      <ul class="flex flex-col gap-3">
+        <li
+          v-for="item in order.orderItems"
+          :key="item.id"
+          class="flex flex-col sm:flex-row gap-3 p-5 border-b border-gray-700"
+        >
+          <div class="w-full sm:w-64 h-40 overflow-hidden">
+            <img
+              src="https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              class="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div>
+            <p class="text-lg font-bold">{{ item.menuItem.name }}</p>
+            <p>
+              Price: <span class="font-bold">{{ formatCurrency(item.menuItem.price) }}</span>
+            </p>
+            <p>
+              Quantity: <span class="font-bold">{{ item.quantity }}</span>
+            </p>
+            <p>
+              Subtotal:
+              <span class="font-bold">{{
+                formatCurrency(item.menuItem.price * item.quantity)
+              }}</span>
+            </p>
+          </div>
+        </li>
+      </ul>
+      <p class="mt-5 text-lg text-right">
+        Total: <span class="font-bold">{{ getOrderTotalAmount(order) }}</span>
+      </p>
+    </div>
+
+    <div class="mt-5 flex gap-3">
+      <button class="w-full p-2 text-white bg-red-600 rounded">Cancel Order</button>
+      <button class="w-full p-2 text-white bg-green-600 rounded">Mark as Completed</button>
+      <button class="w-full p-2 text-white bg-gray-600 rounded">Edit Order</button>
+    </div>
+  </div>
 </template>
