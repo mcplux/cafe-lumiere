8e5cfd62e6461af081a50f96e237f59a61adf408
Juan Martinez <pablomar01@outlook.com>
2025-04-28 11:37:28 -0500

    Create OrderFilters component

---
+++ src/modules/waiter/components/OrderFilters.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { useOrdersStore } from '@/modules/orders/stores/orders.store'
+
+const ordersStore = useOrdersStore()
+</script>
+
+<template>
+  <div class="mt-5">
+    <h3 class="font-bold">Order Status:</h3>
+    <div class="flex flex-col sm:flex-row justify-between mt-2">
+      <div class="flex gap-2 items-center">
+        <input
+          type="checkbox"
+          id="pending"
+          v-model="ordersStore.searchFilters.pending"
+          @change="ordersStore.getTodayOrders"
+        />
+        <label for="pending">Pending</label>
+      </div>
+      <div class="flex gap-2 items-center">
+        <input
+          type="checkbox"
+          id="completed"
+          v-model="ordersStore.searchFilters.completed"
+          @change="ordersStore.getTodayOrders"
+        />
+        <label for="completed">Completed</label>
+      </div>
+      <div class="flex gap-2 items-center">
+        <input
+          type="checkbox"
+          name="paid"
+          id="paid"
+          v-model="ordersStore.searchFilters.paid"
+          @change="ordersStore.getTodayOrders"
+        />
+        <label for="paid">Paid</label>
+      </div>
+      <div class="flex gap-2 items-center">
+        <input
+          type="checkbox"
+          id="cancelled"
+          v-model="ordersStore.searchFilters.cancelled"
+          @change="ordersStore.getTodayOrders"
+        />
+        <label for="cancelled">Cancelled</label>
+      </div>
+    </div>
+  </div>
+</template>
--- src/modules/waiter/views/WaiterOrdersView.vue
+++ src/modules/waiter/views/WaiterOrdersView.vue
@@ -3,6 +3,7 @@ import LoadingSpinner from '@/modules/common/components/LoadingSpinner.vue'
 import OrderList from '@/modules/orders/components/OrderList.vue'
 import { useOrdersStore } from '@/modules/orders/stores/orders.store'
 import { onMounted } from 'vue'
+import OrderFilters from '../components/OrderFilters.vue'
 
 const ordersStore = useOrdersStore()
 
@@ -22,49 +23,7 @@ onMounted(() => {
     </RouterLink>
   </div>
 
-  <!-- Filters -->
-  <div class="mt-5">
-    <h3 class="font-bold">Order Status:</h3>
-    <div class="flex flex-col sm:flex-row justify-between mt-2">
-      <div class="flex gap-2 items-center">
-        <input
-          type="checkbox"
-          id="pending"
-          v-model="ordersStore.searchFilters.pending"
-          @change="ordersStore.getTodayOrders"
-        />
-        <label for="pending">Pending</label>
-      </div>
-      <div class="flex gap-2 items-center">
-        <input
-          type="checkbox"
-          id="completed"
-          v-model="ordersStore.searchFilters.completed"
-          @change="ordersStore.getTodayOrders"
-        />
-        <label for="completed">Completed</label>
-      </div>
-      <div class="flex gap-2 items-center">
-        <input
-          type="checkbox"
-          name="paid"
-          id="paid"
-          v-model="ordersStore.searchFilters.paid"
-          @change="ordersStore.getTodayOrders"
-        />
-        <label for="paid">Paid</label>
-      </div>
-      <div class="flex gap-2 items-center">
-        <input
-          type="checkbox"
-          id="cancelled"
-          v-model="ordersStore.searchFilters.cancelled"
-          @change="ordersStore.getTodayOrders"
-        />
-        <label for="cancelled">Cancelled</label>
-      </div>
-    </div>
-  </div>
+  <OrderFilters />
 
   <LoadingSpinner v-if="ordersStore.isLoading" />
 
