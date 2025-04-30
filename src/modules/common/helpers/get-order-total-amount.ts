0008f941e2e0d053d9f2698aad971499370e1ad6
Juan Martinez <pablomar01@outlook.com>
2025-04-30 09:08:17 -0500

    Fix getOrderTotalAmount helper. Now it includes quantity

--- src/modules/common/helpers/get-order-total-amount.ts
+++ src/modules/common/helpers/get-order-total-amount.ts
@@ -2,7 +2,10 @@ import type { OrderResponse } from '@/modules/orders/interfaces/order.response'
 import { formatCurrency } from './format-currency'
 
 export const getOrderTotalAmount = (order: OrderResponse): string => {
-  const totalAmount = order.orderItems.reduce((total, item) => total + item.menuItem.price, 0)
+  const totalAmount = order.orderItems.reduce(
+    (total, item) => total + item.menuItem.price * item.quantity,
+    0,
+  )
 
   return formatCurrency(totalAmount)
 }
