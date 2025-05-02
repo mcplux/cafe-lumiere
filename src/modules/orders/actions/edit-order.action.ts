b9ce3df89b67028d3e47f94ef7293ad73b1b81de
Juan Martinez <pablomar01@outlook.com>
2025-05-02 11:26:52 -0500

    Handle errors in ediOrderAction

--- src/modules/orders/actions/edit-order.action.ts
+++ src/modules/orders/actions/edit-order.action.ts
@@ -1,3 +1,5 @@
+import { isAxiosError } from 'axios'
+
 import cafeLumiereApi from '@/api/cafe-lumiere.api'
 import type { OrderResponse, OrderStatus } from '../interfaces'
 
@@ -8,8 +10,53 @@ interface EditOrder {
   orderStatus?: OrderStatus
 }
 
-export const editOrderAction = async (id: OrderResponse['id'], order: EditOrder) => {
-  const { data } = await cafeLumiereApi.patch<OrderResponse>(`/orders/${id}`, order)
+type EditOrderResponse =
+  | {
+      ok: true
+      order: OrderResponse
+    }
+  | {
+      ok: false
+      code: number
+      msg: string
+    }
 
-  return data
+export const editOrderAction = async (
+  id: OrderResponse['id'],
+  order: EditOrder,
+): Promise<EditOrderResponse> => {
+  try {
+    const { data } = await cafeLumiereApi.patch<OrderResponse>(`/orders/${id}`, order)
+
+    return {
+      ok: true,
+      order: data,
+    }
+  } catch (error) {
+    if (isAxiosError(error) && error.status === 400) {
+      return {
+        ok: false,
+        code: error.status,
+        msg: 'Order not found',
+      }
+    }
+
+    if (isAxiosError(error) && error.status === 401) {
+      return {
+        ok: false,
+        code: error.status,
+        msg: 'Your session has expired',
+      }
+    }
+
+    if (isAxiosError(error) && error.status === 404) {
+      return {
+        ok: false,
+        code: error.status,
+        msg: 'Order not found',
+      }
+    }
+    console.error(error)
+    throw new Error('Something went wrong while editing order')
+  }
 }
