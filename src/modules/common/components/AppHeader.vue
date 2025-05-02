57b4154a2fb0e44d5aac5b5ee73be050dbe1a953
Juan Martinez <pablomar01@outlook.com>
2025-05-02 15:38:05 -0500

    Change menu to include more links

--- src/modules/common/components/AppHeader.vue
+++ src/modules/common/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import AppMenu from './AppMenu.vue'
 
 interface Props {
   routes: {
@@ -18,55 +18,7 @@ defineProps<Props>()
         Café Lumière
       </RouterLink>
 
-      <div class="text-right">
-        <Menu as="div" class="relative inline-block text-left">
-          <div>
-            <MenuButton
-              class="flex w-full items-center gap-2 rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            </MenuButton>
-          </div>
-
-          <transition
-            enter-active-class="transition duration-100 ease-out"
-            enter-from-class="transform scale-95 opacity-0"
-            enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-in"
-            leave-from-class="transform scale-100 opacity-100"
-            leave-to-class="transform scale-95 opacity-0"
-          >
-            <MenuItems
-              class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
-            >
-              <MenuItem v-slot="{ active }" v-for="route in routes" :key="route.title">
-                <RouterLink
-                  :class="[
-                    active ? 'bg-orange-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                  :to="route.to"
-                >
-                  {{ route.title }}
-                </RouterLink>
-              </MenuItem>
-            </MenuItems>
-          </transition>
-        </Menu>
-      </div>
+      <AppMenu />
     </div>
   </header>
 </template>
---
+++ src/modules/common/components/AppMenu.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useAuthStore } from '@/modules/auth/stores/auth.store'
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import { useRouter } from 'vue-router'
+import {
+  ArrowRightStartOnRectangleIcon,
+  Bars3Icon,
+  BookOpenIcon,
+  HomeIcon,
+  ReceiptPercentIcon,
+} from '@heroicons/vue/16/solid'
+
+interface Route {
+  to: { name: string }
+  title: string
+}
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const routes = ref<Route[]>([
+  { to: { name: 'home' }, title: 'Home' },
+  { to: { name: 'public-menu' }, title: 'Menu' },
+])
+
+const handleLogout = () => {
+  authStore.logout()
+  router.replace({ name: 'login' })
+}
+
+onMounted(async () => {
+  await authStore.checkAuthStatus()
+
+  if (authStore.isWaiter) {
+    routes.value.push({ to: { name: 'waiter-orders' }, title: 'Orders' })
+  }
+})
+</script>
+
+<template>
+  <div class="text-right">
+    <Menu as="div" class="relative inline-block text-left">
+      <div>
+        <MenuButton
+          class="flex w-full items-center gap-2 rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+        >
+          <Bars3Icon class="size-6" />
+        </MenuButton>
+      </div>
+
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <MenuItems
+          class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+        >
+          <MenuItem v-slot="{ active }">
+            <RouterLink
+              :class="[
+                active ? 'bg-orange-500 text-white' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm gap-2',
+              ]"
+              :to="{ name: 'home' }"
+            >
+              <HomeIcon class="size-4" />
+              Home
+            </RouterLink>
+          </MenuItem>
+
+          <MenuItem v-slot="{ active }">
+            <RouterLink
+              :class="[
+                active ? 'bg-orange-500 text-white' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm gap-2',
+              ]"
+              :to="{ name: 'public-menu' }"
+            >
+              <BookOpenIcon class="size-4" />
+              Menu
+            </RouterLink>
+          </MenuItem>
+
+          <MenuItem v-slot="{ active }" v-if="authStore.isWaiter">
+            <RouterLink
+              :class="[
+                active ? 'bg-orange-500 text-white' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm gap-2',
+              ]"
+              :to="{ name: 'waiter-orders' }"
+            >
+              <ReceiptPercentIcon class="size-4" />
+              Orders
+            </RouterLink>
+          </MenuItem>
+
+          <MenuItem v-slot="{ active }" v-if="authStore.isWaiter">
+            <button
+              :class="[
+                active ? 'bg-orange-500 text-white' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm gap-2',
+              ]"
+              @click="handleLogout"
+            >
+              <ArrowRightStartOnRectangleIcon class="size-4" />
+              Logout
+            </button>
+          </MenuItem>
+        </MenuItems>
+      </transition>
+    </Menu>
+  </div>
+</template>
