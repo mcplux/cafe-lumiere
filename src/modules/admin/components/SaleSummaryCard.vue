458ea36c320890102e570cac10f4aa9f07f090e3
Juan Martinez <pablomar01@outlook.com>
2025-05-08 14:45:21 -0500

    Add SummaryCards to AdminDashboard

---
+++ src/modules/admin/components/SaleSummaryCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ChevronDoubleDownIcon, ChevronDoubleUpIcon } from '@heroicons/vue/16/solid'
+
+defineProps<{
+  title: string
+  value: string
+  change: number
+}>()
+</script>
+
+<template>
+  <div class="border border-gray-700 p-5 rounded w-1/3 flex flex-col gap-1 shadow-md">
+    <p class="text-gray-600 text-sm">{{ title }}:</p>
+    <p class="text-xl font-bold">{{ value }}</p>
+    <p
+      class="flex items-center gap-0.5 text-xs"
+      :class="change > 0 ? 'text-green-600' : 'text-red-600'"
+    >
+      <ChevronDoubleUpIcon v-if="change > 0" class="size-4" />
+      <ChevronDoubleDownIcon v-else class="size-4" />
+      {{ change > 0 ? 'Increased' : 'Decreased' }} by {{ change }}%
+    </p>
+  </div>
+</template>
--- src/modules/admin/views/AdminDashboard.vue
+++ src/modules/admin/views/AdminDashboard.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import TitlePage from '@/modules/common/components/TitlePage.vue'
+import SaleSummaryCard from '../components/SaleSummaryCard.vue'
 </script>
 
 <template>
   <TitlePage title="Dashboard" />
+
+  <div>
+    <h2 class="text-lg font-bold">Sales Summary:</h2>
+    <div class="flex gap-3 mt-5">
+      <SaleSummaryCard title="Total Revenue" value="$365.00" :change="32" />
+      <SaleSummaryCard title="Total Orders" value="45" :change="-5" />
+      <SaleSummaryCard title="Total Revenue" value="$8.20" :change="-11" />
+    </div>
+  </div>
 </template>
